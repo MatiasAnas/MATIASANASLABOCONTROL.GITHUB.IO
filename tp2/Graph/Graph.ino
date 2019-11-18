67dbad549cf67105eeaa7d60cf568565cfb79495
jmtome <jtome@fi.uba.ar>
2019-11-18 16:32:05 -0300

    Implementamos PID

--- tp2/Graph/Graph.ino
+++ tp2/Graph/Graph.ino
@@ -10,12 +10,31 @@ volatile unsigned int edgesCount = 0;
 volatile unsigned int edgesCountBuffer = edgesCount;
 
 const unsigned int windowInMillis = 20;
-unsigned int windowCounter = windowInMillis;
+unsigned long windowCounter = windowInMillis;
 
 unsigned int pwmCounter = 1000;
 unsigned int duty = 0;
 
 const int timeSampling = 1;
+
+float Ik = 0;
+
+//float kp = 0.16819;
+//float ki = 37.31;
+
+float kp = 0.13;
+float ki = 5;
+
+uint8_t nkp = 13;
+uint8_t nki = 5;
+uint8_t nkd = 0;
+uint8_t N = 0;
+
+uint8_t reference_high;
+uint8_t reference_low;
+
+float reference = 2000;
+
 void setup() {
   // initialize the serial communication:
   pinMode(pwmOutput, OUTPUT);
@@ -34,43 +53,81 @@ void loop() {
     edgesCount = 0;
 
     //Para enviar a MATLAB.
-    //float RPM = (float)edgesCountBuffer * 50.0 / ((float)ENCODER_PPV * 2.0) * 60;
-    //Serial.write(  highByte( (uint16_t)(RPM) )  );
-    //Serial.write(  lowByte ( (uint16_t)(RPM) )  );
-    //Serial.write(  lowByte( (uint16_t)(duty) )  );
+    float RPM = (float)edgesCountBuffer * 50.0 / ((float)ENCODER_PPV * 2.0) * 60;
+
+    pidUpdate(RPM);
+    pidOutput(RPM);
+
+    if (Serial.available() >= 10) {
+      //char a = Serial.read();
+      //char b = Serial.read();
+      //char c = Serial.read();
+      //char d = Serial.read();
+      nkp = Serial.read();
+      nki = Serial.read();
+      nkd = Serial.read();
+      reference_high = Serial.read();
+      reference_low  = Serial.read();
+      reference = reference_high * 256 + reference_low;
+      N = Serial.read();
+    }
+
+    kp = 0.01 * (float)nkp;
+    ki = 1 * (float)nki;
+
+    Serial.print("efgh");
+    Serial.write(nkp);
+    Serial.write(nki);
+    Serial.write(nkd);
+    Serial.write(  highByte( (uint16_t)(reference) )  );
+    Serial.write(  lowByte ( (uint16_t)(reference) )  );
+    Serial.write(  lowByte ( (uint16_t)(duty) )  );
+    Serial.write(  highByte( (uint16_t)(RPM) )  );
+    Serial.write(  lowByte ( (uint16_t)(RPM) )  );
+
+    /*****************************************************************/
     
     //Para el ide de Arduino.
-    float RPM = (float)edgesCountBuffer * 50.0 / ((float)ENCODER_PPV * 2.0) * 60;
-    //float RPM = (float)edgesCountBuffer;
+    //float RPM = (float)edgesCountBuffer * 50.0 / ((float)ENCODER_PPV * 2.0) * 60;
+
+    //pidUpdate(RPM);
+    //pidOutput(RPM);
+    
 
     //Para identificacion con ide de Arduino.
-    if(2000 > millis()) {
+    /*if(2000 > millis()) {
       Serial.print(millis(), DEC);
       Serial.print(",");
       Serial.print(duty, DEC);
       Serial.print(",");
       Serial.println(RPM, DEC);
-    }
+    }*/
     
     //Para serial plotter del ide de Arduino.
     //Serial.println(RPM, DEC);
-  }
-  if(pwmCounter < millis()) {
-    analogWrite(pwmOutput,255);
-    duty = 255;
-    /*pwmCounter += 1000;
-    if(duty == 128) {
-      duty = 255;
-      analogWrite(pwmOutput,255);
-    } else {
-      duty = 128;
-      analogWrite(pwmOutput,128);
-    }*/
 
-    
+    /*****************************************************************/
   }
 }
 
 void countEdges() {
   edgesCount++;  
+}
+
+void pidUpdate(float RPM) {
+  float Ikp1 = Ik + kp * ki * windowInMillis * (reference - RPM) / 1000;
+  Ik = Ikp1;
+}
+
+void pidOutput(float RPM) {
+  float Pk = kp * (reference - RPM);
+  float output = Pk + Ik;
+  int PWM = round(output);
+  if(PWM > 255) {
+    PWM = 255;
+  } else if(PWM < 0) {
+    PWM = 0;
+  }
+  analogWrite(pwmOutput, PWM);
+  duty = PWM;
 }
